c82815be403937429d352cbbd0605977dea0cb8b
Henkje <henkje@pm.me>
2019-03-09 11:56:20 +0100

    Upload

---
+++ 2FA/2FA/2FAGenerator.cs
@@ -0,0 +1,62 @@
+﻿/* 2FA
+ * Copyright (C) 2019  Henkje (henkje@pm.me)
+ * 
+ * MIT license
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+using System;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace _2FA
+{
+    class _2FAGenerator
+    {     
+        /// <summary>
+        /// Used to Generate 2FA code,
+        /// </summary>
+        byte[] Secret;
+        public _2FAGenerator(string Secret)=>
+            this.Secret = Base32.ToBytes(Secret);
+
+        /// <summary>
+        /// Generate a 2FA code
+        /// </summary>
+        /// <returns>2FA code</returns>
+        /// See: https://tools.ietf.org/html/rfc6238
+        public string GenerateCode()
+        {
+            //Get timestamp.
+            DateTime EPOCH = new DateTime(1970, 1, 1, 0, 0, 0);
+            long Timestamp = Convert.ToInt64(
+                Math.Round((DateTime.UtcNow - EPOCH).TotalSeconds)) / 30;
+
+            //Generate hash.
+            byte[] HMAC = new HMACSHA1(Secret)
+                .ComputeHash(
+                BitConverter.GetBytes(Timestamp).Reverse().ToArray());
+
+            //Generate 6 digit number.
+            int Offset = HMAC.Last() & 0x0F;
+            int Number = (
+               ((HMAC[Offset + 0] & 0x7f) << 24) |
+               ((HMAC[Offset + 1] & 0xff) << 16) |
+               ((HMAC[Offset + 2] & 0xff) << 8) |
+               (HMAC[Offset + 3] & 0xff)) % 1000000;
+
+            //int doesn't start with a 0.
+            //Add the zero's back to it.
+            string _2FACode = Number.ToString();
+            while (_2FACode.Length < 6)
+                _2FACode = '0' + _2FACode;
+
+            return _2FACode;
+        }
+    }
+}
---
+++ 2FA/2FA/Base32.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace _2FA
+{
+    internal class Base32
+    {
+        //Made by: Shane
+        //http://stackoverflow.com/a/7135008/1242
+        //[Edited by henkje]
+        public static byte[] ToBytes(string Input)
+        {
+            Input = Input.TrimEnd('='); //Remove padding characters.
+            int ByteCount = Input.Length * 5 / 8;
+            byte[] ReturnArray = new byte[ByteCount];
+
+            byte CurByte = 0, BitsRemaining = 8;
+            int Mask = 0, ArrayIndex = 0;
+
+            foreach (char c in Input)
+            {
+                int cValue = CharToValue(c);
+
+                if (BitsRemaining > 5)
+                {
+                    Mask = cValue << (BitsRemaining - 5);
+                    CurByte = (byte)(CurByte | Mask);
+                    BitsRemaining -= 5;
+                }
+                else
+                {
+                    Mask = cValue >> (5 - BitsRemaining);
+                    CurByte = (byte)(CurByte | Mask);
+                    ReturnArray[ArrayIndex++] = CurByte;
+                    CurByte = (byte)(cValue << (3 + BitsRemaining));
+                    BitsRemaining += 3;
+                }
+            }
+
+            //When not end with a full byte[].
+            if (ArrayIndex != ByteCount)
+                ReturnArray[ArrayIndex] = CurByte;
+
+            return ReturnArray;
+        }
+
+        private static int CharToValue(char c)
+        {
+            int value = (int)c;
+
+            //Uppercase letters.
+            if (value < 91 && value > 64)
+                return value - 65;
+            //Numbers.
+            else if (value < 56 && value > 49)
+                return value - 24;
+            //Lowercase letters.
+            else if (value < 123 && value > 96)
+                return value - 97;
+            else throw new ArgumentException("Character is not a valid Base32 character.");
+        }
+    }
+}
